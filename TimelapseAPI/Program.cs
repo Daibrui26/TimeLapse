fad5ecd1a2bdc9d813711981ebebefdcc212e950
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-02-11 17:38:01 +0100

    Documentos del logo

--- TimelapseAPI/Program.cs
+++ TimelapseAPI/Program.cs
@@ -3,20 +3,25 @@ using TimelapseAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//
 // 🔹 Controllers
-//
 builder.Services.AddControllers();
 
-//
 // 🔹 Swagger
-//
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//
+// 🔹 CORS - DEBE IR AQUÍ, ANTES DE Build()
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowVueApp",
+        policy => policy
+            .WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials());
+});
+
 // 🔹 Repositories (ADO.NET)
-//
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 builder.Services.AddScoped<ICapsulaRepository, CapsulaRepository>();
 builder.Services.AddScoped<IAmistadRepository, AmistadRepository>();
@@ -25,9 +30,7 @@ builder.Services.AddScoped<IContenidoRepository, ContenidoRepository>();
 builder.Services.AddScoped<INotificacionRepository, NotificacionRepository>();
 builder.Services.AddScoped<IUsuarioCapsulaRepository, UsuarioCapsulaRepository>();
 
-//
 // 🔹 Services
-//
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 builder.Services.AddScoped<ICapsulaService, CapsulaService>();
 builder.Services.AddScoped<IAmistadService, AmistadService>();
@@ -36,24 +39,23 @@ builder.Services.AddScoped<IContenidoService, ContenidoService>();
 builder.Services.AddScoped<INotificacionService, NotificacionService>();
 builder.Services.AddScoped<IUsuarioCapsulaService, UsuarioCapsulaService>();
 
+// AQUÍ SE CONSTRUYE LA APP
 var app = builder.Build();
 
-//
 // 🔹 Middleware
-//
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// ✅ CORS - DEBE IR ANTES DE HTTPS Y AUTORIZACIÓN
+app.UseCors("AllowVueApp");
 
+app.UseHttpsRedirection();
 app.UseAuthorization();
 
-//
 // 🔹 Controllers routing
-//
 app.MapControllers();
 
 app.Run();
